4a2895d5f60b67bbec9bf5daf9bcd96a54a16567
Raven <RavensKrag@gmail.com>
2012-03-11 18:57:50 -0400

    Update event processor to allow for held keys.

--- EventProcessor.py
+++ EventProcessor.py
@@ -1,15 +1,40 @@
-import pygame
-import sys
+# Module for processing pygame events
+import sys, pygame
 
 class EventProcessor(object):
-	def __init__(self):
-		pass
+	def __init__(self, window, player):
+		self.window = window
+		self.player = player
 		
+		self.inputs = {} # Initialize new dictionary
+		
+		self.right_key = pygame.K_e
+		self.left_key = pygame.K_a
+		self.jump_key = pygame.K_COMMA
+	
 	def update(self):
 		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				sys.exit()
 			if event.type == pygame.KEYDOWN:
-				# KeyDown events
+				# Get button press events
 				if event.key == pygame.K_ESCAPE:
 					sys.exit()
+				elif event.key == self.jump_key:
+					self.player.jump()
+					
+				
+				else:	# Get button hold keydowns
+					self.inputs[event.key] = True
+			elif event.type == pygame.KEYUP:
+				self.inputs[event.key] = False
+			elif event.type == pygame.QUIT:
+				sys.exit()
+			#~ elif event.type == pygame.MOUSEBUTTONDOWN:
+				#~ if event.button == 1: # Left Click
+					#~ pos = (event.pos[0]-self.window.offset_x, event.pos[1])
+					#~ self.player.shoot(pos)
+		
+		# Process held buttons
+		if self.inputs.get(self.right_key, False):
+			self.player.move_right()
+		if self.inputs.get(self.left_key, False):
+			self.player.move_left()

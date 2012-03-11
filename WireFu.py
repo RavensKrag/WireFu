8225079da2b40b21aa221acbbb75a73413a250d0
Raven <RavensKrag@gmail.com>
2012-03-11 18:56:28 -0400

    Convert the main file to use an object-oriented style.

--- WireFu.py
+++ WireFu.py
@@ -14,33 +14,52 @@ from gameobjects import *
 import Physics
 from Animation import *
 
-# Main gamestate initialization
-pygame.init()
 
-width, height = dimentions = (600,600)
-
-screen = pygame.display.set_mode(dimentions)
-clock = pygame.time.Clock()
-
-# Initialize systems
-Physics.screen_height = height
-input_processor = EventProcessor()
-
-# Initialize game objects
-gameobjects = pygame.sprite.Group()
-player = Player()
-platforms = [Platform([0,0], [100, 10]),
-			Platform([0,0], [100, 10]),
-			Platform([0,0], [100, 10])]
-
-def update():
-	input_processor.update()
+class Window(object):
+	def __init__(self, width, height):
+		# Main gamestate initialization
+		pygame.init()
+		
+		self.width = width
+		self.height = height
+		self.dimentions = self.width, self.height
+		
+		self.screen = pygame.display.set_mode(self.dimentions)
+		self.clock = pygame.time.Clock()
+		self.framerate = 50
+		
+		self.space = pm.Space()
+		
+		# Initialize game objects
+		self.gameobjects = pygame.sprite.Group()
+		self.player = Player()
+		self.platforms = [Platform([0,0], [100, 10]),
+					Platform([0,0], [100, 10]),
+					Platform([0,0], [100, 10])]
+		
+		# Add objects to space
+		self.player.add_to(self.space)
+		#~ for p in platforms:
+			#~ p.add_to(space)
+		
+		# Initialize systems
+		Physics.screen_height = height
+		self.input_processor = EventProcessor(self, self.player)
 	
-def draw():
-	player.draw(screen)
-
-while 1:
-	update()
-	draw()
-	pygame.display.flip()
-	clock.tick(50)
+	def update(self):
+		self.space.step(self.framerate)
+		self.input_processor.update()
+		
+	def draw(self):
+		self.player.draw(self.screen)
+		for p in self.platforms:
+			p.draw(self.screen)
+	
+	def main(self):
+		while 1:
+			self.update()
+			self.draw()
+			pygame.display.flip()
+			self.clock.tick(self.framerate)
+	
+Window(600,600).main()

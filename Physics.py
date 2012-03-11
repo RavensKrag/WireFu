d69de6e1f5b08e028d09d7002a5108ee454e4cc8
Raven <RavensKrag@gmail.com>
2012-03-11 18:54:30 -0400

    Create properly sized surface and draw a polygon for platform objects.

--- Physics.py
+++ Physics.py
@@ -1,20 +1,26 @@
+#~ import pygame
+#~ from pygame.locals import *
+#~ from pygame.color import *
+#~ import pymunk as pm
+#~ from pymunk import Vec2d
+
 class Physics(object):
 	screen_height = None
 	
-	def to_px(meters):
-		return int(meters)
-	
-	def to_meters(px):
-		return int(p.x)
+	#~ def to_px(self, meters):
+		#~ return int(meters)
+	#~ 
+	#~ def to_meters(self, px):
+		#~ return int(p.x)
 		
-	def to_pygame(vec):
+	def to_pygame(self, vec):
 		# Convert from pymunk coordinates to pygame coordinates
 		return to_px(vec.x), self.screen_height-to_px(vec.y)
 	
-	def to_pymunk(x, y):
-		# Only implement if necessary
-		pass
+	#~ def to_pymunk(self, point):
+		#~ # Only implement if necessary
+		#~ return Vec2d(float(point[0]), float(point[1]))
 	
-	def add_to(space):
+	def add_to(self, space):
 		space.add(self.body, self.shape)
 	
--- gameobjects/gameobject.py
+++ gameobjects/gameobject.py
@@ -38,9 +38,6 @@ class NonstaticObject(GameObject):
 				(-self._animation.get_width()/2, self._animation.get_height())]
 		
 		super(NonstaticObject, self).__init__(body, verts)
-		
-		
-		
 	
 	def update(self):
 		self.image, self.rect = self._animation.update()
@@ -51,7 +48,25 @@ class StaticObject(GameObject):
 	def __init__(self, verts):
 		super(StaticObject, self).__init__(self.static_body, verts)
 		
-		self.image = pygame.Surface([1,1])
+		# Compute the dimensions of a rectangle which will encompass the shape, so that
+		# a Surface of the appropriate size can be generated. 
+		left = right = top = bottom = None
+		for v in verts:
+			if left == None or v[0] < left:
+				left = v[0]
+			if right == None or v[0] > right:
+				right = v[0]
+			if top == None or v[1] > top:
+				top = v[1]
+			if bottom == None or v[1] < bottom:
+				bottom = v[1]
+		
+		self.image = pygame.Surface([right-left, top-bottom])
+		
+		# Draw polygon on image
+		color = pygame.Color("red")
+		pygame.draw.polygon(self.image, color, verts)
+		
 		self.rect = self.image.get_rect()
 		
 	def update(self):

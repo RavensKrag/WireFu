84b59bcf2df935f8c64916478ceac09f77e73ee8
Raven <RavensKrag@gmail.com>
2012-03-12 11:35:06 -0400

    Draw player in the correct location.

--- gameobjects/Player.py
+++ gameobjects/Player.py
@@ -20,7 +20,7 @@ class Player(NonstaticObject):
 		self.rect = self.image.get_rect()
 		self.rect.topleft = [0,0]
 		
-		self.body.velocity_limit = 6
+		self.body.velocity_limit = 8
 		
 		self.body.position.x = 0
 		self.body.position.y = 1
@@ -31,11 +31,12 @@ class Player(NonstaticObject):
 		self.in_air = False
 		
 		self.movement_force = Vec2d(250, 0.0)
-		self.air_movement_force = Vec2d(110, 0.0)
+		self.air_movement_force = Vec2d(120, 0.0)
 	
 	def draw(self, screen):
-		pos = self.to_pygame(self.body.position)
-		screen.blit(self.image, (pos[0], pos[1]-self._animation.get_height()))
+		pos = Physics.to_pygame(self.body.position)
+		screen.blit(self.image, (pos[0]-self._animation.get_width()/2, 
+								pos[1]-self._animation.get_height()))
 	
 	def update(self, window_width):
 		super(Player, self).update()
@@ -44,19 +45,19 @@ class Player(NonstaticObject):
 			self.body.position.y = 0
 			
 		#~ print self.body.position
-		#~ print self.to_pygame(self.body.position)
+		#~ print Physics.to_pygame(self.body.position)
 		#~ print self.rect
 		
 		# Constrain movement of player to screen
 		window_width = Physics.to_meters(window_width)
 		width = Physics.to_meters(self._animation.get_width())
 		#~ print self.body.force
-		if(self.x < 0):
-			self.x = 0
+		if(self.x < width/2):
+			self.x = width/2
 			self.body.force.x = 0
 			self.body.velocity.x = 0
-		elif(self.x + width > window_width):
-			self.x = window_width - width
+		elif(self.x + width/2 > window_width):
+			self.x = window_width - width/2
 			self.body.force.x = 0
 			self.body.velocity.x = 0
 		
@@ -77,7 +78,7 @@ class Player(NonstaticObject):
 	def jump(self):
 		if self.jump_count < self.jump_limit:
 			self.in_air = True
-			self.body.velocity.y += 5
+			self.body.velocity.y += 5	# Add instead of setting to take momentum into account
 			self.jump_count += 1
 	
 	def ground_collision(self):

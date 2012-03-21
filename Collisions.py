0dc8ffb55bb7426d4a948fc2fd2f47e6cf46e466
Raven <RavensKrag@gmail.com>
2012-03-21 02:33:04 -0400

    Correct collisions between the player and the ground.

--- Collisions.py
+++ Collisions.py
@@ -134,16 +134,19 @@ class GroundCollision(object):
 	
 	@staticmethod
 	def pre_solve(space, arbiter):
-		#~ a, b = arbiter.shapes
-		
 		a, b = arbiter.shapes
+		#~ player_shape, env_shape = arbiter.shapes
 		
+		
+		#~ if(player_shape.body.velocity.y < 0 and player_shape.body.position.y > env_shape.body.position.y):
+			# If moving downwards from above
+			#~ player_shape.gameobject.ground_collision()
 		if(a.collision_type == PLAYER):
 			#~ print "player a"
 			a.gameobject.ground_collision()
-		elif(b.collision_type == PLAYER):
+		#~ elif(b.collision_type == PLAYER):
 			#~ print "player b"
-			b.gameobject.ground_collision()
+			#~ b.gameobject.ground_collision()
 		
 		return True
 	
